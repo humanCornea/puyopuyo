e636f39d6565ca8789c5ccbaf92576cc8c72cd61
meko3 <hirono.yoneyama@outlook.com>
2019-01-03 23:13:08 +0900

    [feat] add poyoManager

---
+++ Assets/Scripts/poyoManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poyoManager : MonoBehaviour
+{
+    GameObject prefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game start");
+        this.prefabs = (GameObject)Resources.Load("Prefabs/poyo");
+        Instantiate(this.prefabs);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Instantiate(this.prefabs);   
+    }
+}

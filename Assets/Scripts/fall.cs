c0fd0e353e1e4b100716973aeb8699626c54fc70
meko3 <hirono.yoneyama@outlook.com>
2019-01-03 22:05:22 +0900

    [feat] add fall stopper

--- Assets/Scripts/fall.cs
+++ Assets/Scripts/fall.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class fall : MonoBehaviour
 {
+    float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.speed = -2.0f * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Translate(0, -0.05f, 0);
+        this.transform.Translate(0, this.speed, 0);
+        this.KeyOperation();
+    }
+
+    void KeyOperation()
+    {
+        if (Input.GetKey("left")) {
+            this.transform.Translate(-0.1f, 0, 0);
+        }
+        if (Input.GetKey("right")) {
+            this.transform.Translate(0.1f, 0, 0);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        this.speed = 0;
     }
 }

376f50145526df55e865a7edb0ceade30a47fd1f
Cornea <ryousei-hatyu-love@hotmail.co.jp>
2019-01-04 14:07:08 +0900

    [feat]add puyoIniposi and quaternion

--- Assets/Scripts/poyoManager.cs
+++ Assets/Scripts/poyoManager.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class poyoManager : MonoBehaviour
 {
     GameObject prefabs;
+    Quaternion q = Quaternion.identity;
+    Vector3 poyoIniposi = new Vector3(0,2,0);
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Game start");
         this.prefabs = (GameObject)Resources.Load("Prefabs/poyo");
-        Instantiate(this.prefabs);   
+        Instantiate(this.prefabs,poyoIniposi,q);   
     }
 
     // Update is called once per frame

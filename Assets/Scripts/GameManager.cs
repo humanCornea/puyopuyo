98839f0a7021ba1fe249996741c12a564bf09061
meko3 <hirono.yoneyama@outlook.com>
2019-01-05 14:05:33 +0900

    [feat] add check poyo conflict

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // stage
+    GameObject stage;
+    public Sprite UISprite;
+    public Color stageColor;
+    public Material stageMaterial;
+    public SpriteDrawMode slicedMode;
+
+    private GameObject prefabs;
+    private Quaternion q = Quaternion.identity;
+    private Vector3 poyoIniposi;
+    private int num;
+
+    float poyoWidth = GameObject.Find("poyo(Clone)").gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+
+    float floorWidth;
+    float floorHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game start");
+        this.genStage();
+        poyoIniposi = this.transform.position + new Vector3(0, 2, 1);
+        this.prefabs = (GameObject)Resources.Load("Prefabs/poyo");
+        Instantiate(this.prefabs,poyoIniposi,q);
+        this.num = 1; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( this.num > 10 ) {
+            Application.Quit();
+        }
+    }
+
+    void genStage() {
+        // make main stage
+        this.stage = new GameObject("Field");
+        this.stage.transform.position = new Vector3(0, 0.01f, 0);
+        this.stage.transform.localScale = new Vector3(12, 24, 1);
+        SpriteRenderer sprite = GameObject.Find("Field").AddComponent<SpriteRenderer>();
+        sprite.sprite = UISprite;
+        sprite.color = stageColor;
+        sprite.material = stageMaterial;
+        sprite.drawMode = slicedMode;
+        sprite.size = new Vector2(0.16f, 0.16f);
+
+        // make left wall
+        BoxCollider2D leftWall = GameObject.Find("Field").AddComponent<BoxCollider2D>();
+        leftWall.offset = new Vector2(-50.08f, 0);
+        leftWall.size = new Vector2(100, 0.2f);
+
+        // make right wall
+        BoxCollider2D rightWall = GameObject.Find("Field").AddComponent<BoxCollider2D>();
+        rightWall.offset = new Vector2(50.08f, 0);
+        rightWall.size = new Vector2(100, 0.2f);
+    }
+
+    public void genPoyo(Collision2D other) {
+        if ( other.gameObject.name == "poyo(Clone)" || other.gameObject.name == "Floor") {
+            this.num--;
+            if ( this.num == 0 ) {
+                this.num++; 
+                Instantiate(this.prefabs, this.poyoIniposi, this.q); 
+            }  
+        }
+    }
+}
---
+++ Assets/Scripts/Poyo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poyo : MonoBehaviour
+{
+    float speed;
+    float width;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.speed = -2.0f * Time.deltaTime;
+        this.width = GameObject.Find("poyo(Clone)").gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(0, this.speed, 0);
+        if(this.speed != 0f)
+        {
+            this.KeyOperation();
+        }
+    }
+
+    void KeyOperation()
+    {
+        if ( Time.frameCount % 10 == 9 ) {
+            if (Input.GetKey("left")) {
+                this.transform.Translate(-this.width, 0, 0);
+            }
+            if (Input.GetKey("right")) {
+                this.transform.Translate(this.width, 0, 0);
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if ( this.GetComponent<Rigidbody2D>().constraints != RigidbodyConstraints2D.FreezeAll ) {
+            if ( other.gameObject.name == "Floor" || other.gameObject.name == "poyo(Clone)"  ) {
+                this.speed = 0;
+                this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+                GameObject.Find("Main Camera").GetComponent<GameManager>().genPoyo(other);
+            }
+        }
+    }
+}
